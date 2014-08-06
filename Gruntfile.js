57040d69f3290dd6af56b8e5ab0df5a3978e3cea
Ivan Vesely <ivan.jan.vesely@gmail.com>
2014-08-06 22:48:20 +0200

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          /*compress: true,
+          yuicompress: true,*/
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          // target.css file: source.less file
+          "assets/css/spylight.min.css": "assets/less/spylight/spylight.less"
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['assets/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    },
+    // grunt-open will open your browser at the project's URL
+    open: {
+        all: {
+            // Gets the port from the connect configuration
+            path: 'http://localhost:9001/'
+        }
+    },
+    connect: {
+        server: {
+            options: {
+                port: 9001,
+                base: '/Workspace/spylight/frontend'
+            }
+        }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-open');
+
+  grunt.registerTask('default', ['connect', 'open', 'watch']);
+};
